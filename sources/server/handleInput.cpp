96b8d1b217992cbc9c563ff741f9d9a88f397c28
lucocozz <lucocozz@student.42.fr>
2022-03-28 23:28:33 +0200

    fix leaks

--- sources/server/handleInput.cpp
+++ sources/server/handleInput.cpp
@@ -6,7 +6,7 @@
 /*   By: lucocozz <lucocozz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 21:55:31 by lucocozz          #+#    #+#             */
-/*   Updated: 2022/03/22 21:56:16 by lucocozz         ###   ########.fr       */
+/*   Updated: 2022/03/26 15:13:19 by lucocozz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@
 #include "request.hpp"
 #include "response.hpp"
 
-static std::pair<bool,LocationContext>  getLocation(std::string path, std::vector<LocationContext> serverLocation){
+static std::pair<bool,LocationContext>  getLocation(std::string path, std::vector<LocationContext> serverLocation) {
 	std::pair<bool,LocationContext> locationPair = std::make_pair(true, serverLocation[0]);
 
-	for (size_t i = 0; i < serverLocation.size(); i++){
-		if (path.find(serverLocation[i].args[0]) != std::string::npos){
+	for (size_t i = 0; i < serverLocation.size(); i++) {
+		if (path.find(serverLocation[i].args[0]) != std::string::npos) {
 			locationPair.second = serverLocation[i];
 			return(locationPair);
 		}
@@ -41,7 +41,7 @@ void	handleInput(EpollSocket &client, const Config &serverConfig)
 	//serverConfig.servers[0] ->  will change depending on what server we work on
 	std::pair<bool,LocationContext> locationResult = getLocation(request.getPath(), serverConfig.servers[0].locations);
 	if (locationResult.first != false){
-		try{
+		try {
 			//serverConfig.servers[0] ->  will change depending on what server we work on
 			std::pair<ServerContext, LocationContext > serverLocation = 
 			std::make_pair(serverConfig.servers[0], locationResult.second);
