57737971ea88e12802423f34abcc41cb1d41457a
lucocozz <lucocozz@student.42.fr>
2022-04-26 16:48:44 +0200

    fix throw transpoting end point

--- sources/server/handling/handleDeconnection.cpp
+++ sources/server/handling/handleDeconnection.cpp
@@ -6,7 +6,7 @@
 /*   By: lucocozz <lucocozz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 21:59:57 by lucocozz          #+#    #+#             */
-/*   Updated: 2022/04/09 21:15:16 by lucocozz         ###   ########.fr       */
+/*   Updated: 2022/04/26 15:34:04 by lucocozz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@
 void	handleDeconnection(std::map<int, Client> &clientList, EpollSocket &socketEvent, Epoll &epoll)
 {
 	epoll.control(EPOLL_CTL_DEL, socketEvent);
-	socketEvent.shutdownSocket();
 	socketEvent.closeSocket();
 	clientList.erase(socketEvent.listener());
 }
